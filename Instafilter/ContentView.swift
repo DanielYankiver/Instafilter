4e383827e655faba2b64e6556231ffc6b0d7cb58
Daniel Yankiver <dyankiver@gmail.com>
2024-01-02 10:33:56 -0500

    content unavailable view

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,37 +5,20 @@
 //  Created by Daniel Yankiver on 1/2/24.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-  @State private var image: Image?
-
   var body: some View {
-    VStack {
-      image?
-        .resizable()
-        .scaledToFit()
+    ContentUnavailableView {
+      Label("No snippets", systemImage: "swift")
+    } description: {
+      Text("You don't have any saved snippets yet")
+    } actions: {
+      Button("Create snippet") {
+        // create snippet
+      }
+      .buttonStyle(.borderedProminent)
     }
-    .onAppear(perform: loadImage)
-  }
-
-  func loadImage() {
-    let inputImage = UIImage(resource: .example)
-    let beginImage = CIImage(image: inputImage)
-
-    let context = CIContext()
-    let currentFilter = CIFilter.sepiaTone()
-
-    currentFilter.inputImage = beginImage
-    currentFilter.intensity = 1
-
-    guard let outputImage = currentFilter.outputImage else { return }
-    guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-
-    let uiImage = UIImage(cgImage: cgImage)
-    image = Image(uiImage: uiImage)
   }
 }
 
