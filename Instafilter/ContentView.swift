9293bce2699efdeb01c8c1902d5f8be7a889e2d3
Daniel Yankiver <dyankiver@gmail.com>
2024-01-02 15:33:05 -0500

    sharing content with ShareLink

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,37 +5,14 @@
 //  Created by Daniel Yankiver on 1/2/24.
 //
 
-import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-  @State private var pickerItems = [PhotosPickerItem]()
-  @State private var selectedImages = [Image]()
-
   var body: some View {
-    VStack {
-      PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .images) {
-        Label("Select a picture", systemImage: "photo")
-      }
+    let example = Image(.example)
 
-      ScrollView {
-        ForEach(0..<selectedImages.count, id: \.self) { i in
-          selectedImages[i]
-            .resizable()
-            .scaledToFit()
-        }
-      }
-    }
-    .onChange(of: pickerItems) {
-      Task {
-        selectedImages.removeAll()
-
-        for item in pickerItems {
-          if let loadedImage = try await item.loadTransferable(type: Image.self) {
-            selectedImages.append(loadedImage)
-          }
-        }
-      }
+    ShareLink(item: example, preview: SharePreview("AT&T offices", image: example)) {
+      Label("Click to share", systemImage: "airplane")
     }
   }
 }
