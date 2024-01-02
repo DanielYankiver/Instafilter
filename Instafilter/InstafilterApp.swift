2f3af67953f7b3f23e2294a690de315648d3a63a
Daniel Yankiver <dyankiver@gmail.com>
2024-01-02 09:31:58 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Daniel Yankiver on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
