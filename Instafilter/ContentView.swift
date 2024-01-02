9a86938db72618858593dcdf10dd9c5b74061256
Daniel Yankiver <dyankiver@gmail.com>
2024-01-02 10:28:30 -0500

    added sepia filter with core images

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,20 +5,40 @@
 //  Created by Daniel Yankiver on 1/2/24.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var image: Image?
+
+  var body: some View {
+    VStack {
+      image?
+        .resizable()
+        .scaledToFit()
     }
+    .onAppear(perform: loadImage)
+  }
+
+  func loadImage() {
+    let inputImage = UIImage(resource: .example)
+    let beginImage = CIImage(image: inputImage)
+
+    let context = CIContext()
+    let currentFilter = CIFilter.sepiaTone()
+
+    currentFilter.inputImage = beginImage
+    currentFilter.intensity = 1
+
+    guard let outputImage = currentFilter.outputImage else { return }
+    guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+    let uiImage = UIImage(cgImage: cgImage)
+    image = Image(uiImage: uiImage)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
