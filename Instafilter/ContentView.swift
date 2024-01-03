fe390c0bafdf2d11872a576eb3c4de1625e51e6d
Daniel Yankiver <dyankiver@gmail.com>
2024-01-03 12:29:08 -0500

    importing an imageI using PhotosPicker

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,24 +5,31 @@
 //  Created by Daniel Yankiver on 1/2/24.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
   @State private var processedImage: Image?
   @State private var filteredIntensity = 0.5
 
+  @State private var selectedItem: PhotosPickerItem?
+
   var body: some View {
     NavigationStack {
       VStack {
         Spacer()
-
-        if let processedImage {
-          processedImage
-            .resizable()
-            .scaledToFit()
-        } else {
-          ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+        
+        PhotosPicker(selection: $selectedItem) {
+          if let processedImage {
+            processedImage
+              .resizable()
+              .scaledToFit()
+          } else {
+            ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+          }
         }
+        .buttonStyle(.plain)
+        .onChange(of: selectedItem, loadImage)
 
         Spacer()
 
@@ -46,6 +53,15 @@ struct ContentView: View {
 
   func changeFilter() {
 
+  }
+
+  func loadImage() {
+    Task {
+      guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+      guard let inputImage = UIImage(data: imageData) else { return }
+
+      // more code to come
+    }
   }
 }
 
